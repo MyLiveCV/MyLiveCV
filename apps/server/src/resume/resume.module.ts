93bbc58e8524805a6cbd19185b156bd385840874
Anubhav Singh <anubhav.singh.e@thalesdigital.io>
2024-05-12 14:37:15 +0530

    lint

--- apps/server/src/resume/resume.module.ts
+++ apps/server/src/resume/resume.module.ts
@@ -3,8 +3,8 @@ import { Module } from "@nestjs/common";
 import { AuthModule } from "@/server/auth/auth.module";
 import { PrinterModule } from "@/server/printer/printer.module";
 
-import { StorageModule } from "../storage/storage.module";
 import { PaymentModule } from "../payment/payment.module";
+import { StorageModule } from "../storage/storage.module";
 import { ResumeController } from "./resume.controller";
 import { ResumeService } from "./resume.service";
 
